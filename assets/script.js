72fab1f26bfdd116690a646682015c0b6527d933
TorySnopl <Paramedic064@gmail.com>
2023-05-14 16:31:37 -0700

    added script

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+let topic = "HTML";
+
+if (topic === 'HTML') {
+    console.log("Let's Study HTML");
+} else if (topic === 'CSS') {
+    console.log('Lets Study CSS');
+} else if (topic === 'Git') {
+    console.log("lets study git");
+} else if (topic === 'Javascript') {
+    console.log("lets study javascript")
+} else {
+    console.log('please try again')
+}
+
